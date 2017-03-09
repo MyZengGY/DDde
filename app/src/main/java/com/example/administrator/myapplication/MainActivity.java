9e180e67ed715339a6b2fb91391d909bb6155ab8
Administrator <1076533734@qq.com>
2017-03-09 14:17:05 +0800

    zuizao

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.textView)
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        onClick();
+textView.setText("儿子你哈皮");
+    }
+
+    @OnClick(R.id.textView)
+    public void onClick() {
+        Toast.makeText(this, "ssss", Toast.LENGTH_SHORT).show();
+    }
+}
